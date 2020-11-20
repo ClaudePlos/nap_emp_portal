00d25143c9a6f23a1da2afcc79ad71fb76295e46
Klaudiusz Skowroński <claude-plos@o2.pl>
2020-11-20 15:49:30 +0100

    fast update

--- src/main/java/pl/kskowronski/data/service/egeria/ek/ZatrudnienieRepo.java
+++ src/main/java/pl/kskowronski/data/service/egeria/ek/ZatrudnienieRepo.java
@@ -1,17 +1,10 @@
 package pl.kskowronski.data.service.egeria.ek;
 
-import javafx.concurrent.Worker;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.kskowronski.data.entity.egeria.ek.Zatrudnienie;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public interface ZatrudnienieRepo extends JpaRepository<Zatrudnienie, BigDecimal> {
-
-
-
 
 }
