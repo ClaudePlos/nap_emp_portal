2ad55c3eb7c3e7c905c569cf6e777e9d0ffb99eb
Klaudiusz Skowroński <claude-plos@o2.pl>
2020-12-14 16:11:11 +0100

    identification

--- src/main/java/pl/kskowronski/data/entity/egeria/ek/User.java
+++ src/main/java/pl/kskowronski/data/entity/egeria/ek/User.java
@@ -18,10 +18,10 @@ public class User {
     @Column(name = "PRC_NUMER")
     private BigDecimal prcNumer;
 
-    @Column(name = "PRC_PESEL")
+    @Column(name = "PRC_IDENTYFIKACJA")
     private String password;
 
-    @Column(name = "PRC_PESEL", insertable=false, updatable=false)
+    @Column(name = "PRC_PESEL")
     private String prcPesel;
 
     @Column(name = "PRC_IMIE")
