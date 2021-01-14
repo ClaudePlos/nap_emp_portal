73a01ecb61aaeeeccdcc9e5b80f69184481c115a
Klaudiusz Skowroński <claude-plos@o2.pl>
2021-01-14 12:10:36 +0100

    update pattern and status pit equals 50

--- src/main/java/pl/kskowronski/data/service/egeria/eDek/EdktDeklaracjeRepo.java
+++ src/main/java/pl/kskowronski/data/service/egeria/eDek/EdktDeklaracjeRepo.java
@@ -15,7 +15,9 @@ public interface EdktDeklaracjeRepo extends JpaRepository<EdktDeklaracje, BigDec
 
     Optional<EdktDeklaracje> findByDklId(BigDecimal dklId);
 
-    @Query("select e from EdktDeklaracje e where e.dklPrcId = :prcId and e.dklDataOd>= :dateFrom and e.dklDataDo <= :dateTo order by e.dklDataOd desc")
+    @Query("select e from EdktDeklaracje e where e.dklPrcId = :prcId and e.dklDataOd>= :dateFrom and e.dklDataDo <= :dateTo " +
+            "and e.dklStatus in (50)" +
+            "order by e.dklDataOd desc")
     Optional<List<EdktDeklaracje>> findAllByDklPrcIdForYear(@Param("prcId") BigDecimal prcId
             , @Param("dateFrom") Date dateFrom
             , @Param("dateTo") Date dateTo);
