5827037b62e0e98e0267dc2c1acab80057281fcf
Klaudiusz Skowroński <claude-plos@o2.pl>
2021-02-09 16:31:21 +0100

    add new feature for logout

--- src/main/java/pl/kskowronski/views/login/LogoutView.java
+++ src/main/java/pl/kskowronski/views/login/LogoutView.java
@@ -6,19 +6,19 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinSession;
 
 @Route("/logout")
 @PageTitle("Logout | Rekeep")
 public class LogoutView extends VerticalLayout {
 
     public LogoutView() {
-
         add(new H1("Rekeep logout"));
-        //UI.getCurrent().getPage().reload();
 
+        VaadinSession.getCurrent().getSession().invalidate();
         UI.getCurrent().getPage().executeJs("window.location.href='logout'");
         // Close the session
         UI.getCurrent().getSession().close();
-
+        UI.getCurrent().close();
     }
 }
