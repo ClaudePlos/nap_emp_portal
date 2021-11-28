2da7be9ad8aacf1f2860499fc30de3c96d4f361a
Klaudiusz Skowroński <klaudsys@gmail.com>
2021-11-28 14:11:27 +0100

    added auto click

--- src/main/java/pl/kskowronski/views/pit11/Pit11View.java
+++ src/main/java/pl/kskowronski/views/pit11/Pit11View.java
@@ -1,16 +1,22 @@
 package pl.kskowronski.views.pit11;
 
 import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Page;
 import com.vaadin.flow.component.splitlayout.SplitLayout;
 import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.data.renderer.NativeButtonRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -153,9 +159,15 @@ public class Pit11View extends VerticalLayout {
         dialog.setHeight("150px");
 
         StreamResource res = new StreamResource("file.pdf", () -> new ByteArrayInputStream(pdfBytes));
+        String timeStamp = "report12";
         Anchor a = new Anchor(res, "kliknij tu by pobrać pit11");
+        a.setId(timeStamp);
+        a.getElement().getStyle().set("display", "none");
         a.setTarget( "_blank" );
         a.getElement().addEventListener("click", event -> {dialog.close();});
+
+        Page page = UI.getCurrent().getPage();
+        page.executeJavaScript("document.getElementById('"+timeStamp+"').click();");
 
         dialog.add(a, new Html("<div><br><div>"), new Button("Zamknij", e -> dialog.close()));
         add(dialog);
