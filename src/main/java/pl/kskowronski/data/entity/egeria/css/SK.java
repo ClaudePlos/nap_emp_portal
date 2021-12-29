0859bb931b97161162c195f083833e14168b3d70
Klaudiusz Skowroński <klaudsys@gmail.com>
2021-12-29 21:00:58 +0100

    added CRUD full v02

--- src/main/java/pl/kskowronski/data/entity/egeria/css/SK.java
+++ src/main/java/pl/kskowronski/data/entity/egeria/css/SK.java
@@ -1,5 +1,7 @@
 package pl.kskowronski.data.entity.egeria.css;
 
+import com.vaadin.flow.component.HasValue;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
--- src/main/java/pl/kskowronski/data/service/egeria/css/SKService.java
+++ src/main/java/pl/kskowronski/data/service/egeria/css/SKService.java
@@ -10,14 +10,22 @@ import pl.kskowronski.data.service.egeria.ckk.ClientRepo;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class SKService extends CrudService<SK, BigDecimal> {
+
+    private List<SK> list  = new ArrayList<>();
 
     private SKRepo repo;
 
     public SKService(@Autowired SKRepo repo) {
         this.repo = repo;
+
+        SK sk = new SK(BigDecimal.valueOf(100725), "AINF","Dział Informatyczny");
+        SK sk1 = new SK(BigDecimal.valueOf(108469),"AKCF","Kadry Cała Firma");
+        list.add(sk);
+        list.add(sk1);
     }
 
     @Override
@@ -25,13 +33,14 @@ public class SKService extends CrudService<SK, BigDecimal> {
         return repo;
     }
 
+
     public List<SK> findAll(){
-        List<SK> list  = new ArrayList<>();
-        SK sk = new SK(BigDecimal.valueOf(100725), "AINF","Dział Informatyczny");
-        SK sk1 = new SK(BigDecimal.valueOf(108469),"AKCF","Kadry Cała Firma");
-        list.add(sk);
-        list.add(sk1);
         return list;
+        //return repo.findAll();
+    }
+
+    public SK findBySkKod( String skKod){
+        return list.stream().filter( item -> item.getSkKod().equals(skKod)).collect(Collectors.toList()).get(0);
         //return repo.findAll();
     }
 
